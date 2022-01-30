1eb01c8181e9ccc342bee420e7ecf1f4d5271a24
Judith <72439973+judicodes@users.noreply.github.com>
2022-01-30 19:56:11 +0100

    Tidy up (#48)
    * Modify GH Actions
    
    * Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
@@ -10,13 +12,18 @@ module.exports = {
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
+
   parserOptions: {
     ecmaVersion: 2020
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/no-loss-of-precision": "off",
+    "vue/multi-word-component-names": "off"
   },
+
   overrides: [
     {
       files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
