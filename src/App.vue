0cc88278a5bb5ebf428ca8c47431cf90ceb39ba5
Judith <72439973+judicodes@users.noreply.github.com>
2022-03-27 19:07:23 +0200

    Add Ingredients view [#5]

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div
-    class="bg-lighttheme dark:bg-darktheme bg-fixed bg-center bg-cover h-full w-full dark:text-offwhite transition duration-700 ease-in-out"
-  >
-    <NavigationBar />
-    <div class="pt-20">
+  <div class="absolute pt-28 top-0 bottom-0 bg-fixed bg-center bg-cover w-full dark:text-offwhite">
+    <NavigationBar
+      class="h-28 -mt-28 -top-14 sticky bg-lighttheme-dark dark:bg-darktheme-dark transition duration-700 ease-in-out"
+    />
+    <div class="min-h-full bg-lighttheme dark:bg-darktheme transition duration-700 ease-in-out pt-20">
       <router-view />
     </div>
-    <Footer />
+    <Footer class="h-14 -mb-14 bg-lighttheme dark:bg-darktheme transition duration-700 ease-in-out" />
     <LegalStuff />
   </div>
 </template>
---
+++ src/views/Ingredients.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Hellooooo</div>
+</template>
