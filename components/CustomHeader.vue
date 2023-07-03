418c1156ed91aa3210efb7d05b2f94305a798707
Judith <72439973+judicodes@users.noreply.github.com>
2023-07-03 17:35:33 +0200

    Add links with icons to CustomHeader.

--- components/CustomHeader.vue
+++ components/CustomHeader.vue
@@ -7,12 +7,60 @@
     </v-row>
     <v-row justify="center">
       <v-col cols="12" align="center">
-        <h1 class="name pa-4">Judith Boehlert</h1>
+        <h1 class="name">Judith Boehlert</h1>
         <h2 class="subtitle">Software Engineer & Solopreneur.</h2>
+        <v-row justify="center" class="pa-5">
+          <a
+            v-for="icon in icons"
+            :key="icon.iconString"
+            :href="icon.url"
+            target="_blank"
+            :title="icon.title"
+          >
+            <v-icon :icon="icon.iconString" size="36px" class="mx-3" />
+          </a>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script lang="ts">
+interface IconLink {
+  iconString: string;
+  url: string;
+  title: string;
+}
+
+export default defineComponent({
+  data() {
+    return {
+      icons: [
+        {
+          iconString: 'fa-brands fa-linkedin',
+          url: 'https://linkedin.com/in/jboehlert',
+          title: 'LinkedIn'
+        },
+        {
+          iconString: 'fa-brands fa-square-instagram',
+          url: 'https://www.instagram.com/judi_codes/',
+          title: 'Instagram @judi_codes'
+        },
+        {
+          iconString: 'fa-brands fa-square-github',
+          url: 'https://github.com/judicodes',
+          title: 'GitHub'
+        },
+        {
+          iconString: 'fas fa-envelope',
+          url: 'mailto:hello@judithboehlert.com',
+          title: 'Send me an email'
+        }
+      ] as IconLink[]
+    };
+  }
+});
+</script>
 
 <style scoped lang="scss">
 .name {
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,13 +1,25 @@
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
-import '@mdi/font/css/materialdesignicons.css';
+import { aliases, fa } from 'vuetify/iconsets/fa-svg';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// eslint-disable-next-line import/named
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faInstagramSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     icons: {
-      defaultSet: 'mdi'
+      defaultSet: 'fa',
+      aliases,
+      sets: { fa }
     }
   });
+  nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon);
+  library.add(faLinkedin);
+  library.add(faInstagramSquare);
+  library.add(faGithubSquare);
+  library.add(faEnvelope);
 
   nuxtApp.vueApp.use(vuetify);
 });
