ba7b7f86515d794d78e3a5b8dbdfc311c1c778f3
Judith Boehlert <72439973+judicodes@users.noreply.github.com>
2023-08-14 16:24:11 +0200

    Fix a11y (#506)
    * Improve a11y.
    
    * Add tags for SEO improvement.
    
    * No CodeQL analysis on PRs.

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
       <v-btn
         class="theme-icon"
         size="x-large"
+        title="Toggle dark/light theme"
         :icon="toggleThemeIcon"
         variant="plain"
         :color="toggleThemeIconColour"
@@ -24,6 +25,25 @@
 <script setup lang="ts">
 import { mdiWhiteBalanceSunny, mdiMoonWaningCrescent } from '@mdi/js';
 import { useTheme } from 'vuetify/lib/framework.mjs';
+
+useServerSeoMeta({
+  title: 'Judith Boehlert - Freelance Software Engineer',
+  ogTitle: 'Judith Boehlert - Freelance Software Engineer',
+  description: 'Freelance Full-stack Software Engineer and Tech Content Creator based in Berlin.',
+  ogDescription: 'Freelance Full-stack Software Engineer and Tech Content Creator based in Berlin.',
+  ogUrl: 'https://judithboehlert.com',
+  ogImage: {
+    url: 'website-thumbnail.png',
+    width: 1200,
+    height: 630
+  }
+});
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  }
+});
 
 const customHeaderKey = ref(0);
 const theme = useTheme();
